dd6bd07d8de46c72af1388fc3333e27616510d3a
ChungSik_Shin <65951253+masin556@users.noreply.github.com>
2022-02-19 17:03:26 +0900

    [Bronze IV] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498.시험 성적/시험 성적.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+	int score;
+	scanf("%d", &score);
+
+	if (90 <= score) {
+		printf("A");
+	}
+	else if (80 <= score) {
+		printf("B");
+	}
+	else if (70 <= score) {
+		printf("C");
+	}
+	else if (60 <= score) {
+		printf("D");
+	}else 
+		printf("F");
+    return 0;
+}
