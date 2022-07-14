4a09c57c15ad930fcf8f692196ef13ce29ee7a86
JohnSik <65951253+masin556@users.noreply.github.com>
2022-07-15 02:57:54 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -1,14 +1,15 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
- 
-int main(){
- 
-    cout << "         ,r'\"7\n";
-    cout << "r`-_   ,'  ,/\n";
-    cout << " \\. \". L_r'\n";
-    cout << "   `~\\/\n";
-    cout << "      |\n";
-    cout << "      |\n";
- 
-    return 0;
+
+int main()
+{
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/ \n";
+	cout << "      |\n";
+	cout << "      |\n";
+	
+	return 0;
 }
