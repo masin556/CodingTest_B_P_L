d7f08cbcedc4fe0d6c0c0bdd90584c7fae6038f0
ppatabox <65951253+masin556@users.noreply.github.com>
2023-06-01 02:30:47 +0900

    Update 두 수의 연산값 비교하기.cpp

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.cpp
@@ -1,8 +1,7 @@
 #include <string>
-#include <vector>
-
+//#include<algorithm>
 using namespace std;
 
 int solution(int a, int b) {
-    return max(stoi(to_string(a) + to_string(b)), (2 * a * b));;
+    return max(stoi(to_string(a) + to_string(b)), (2 * a * b));
 }
