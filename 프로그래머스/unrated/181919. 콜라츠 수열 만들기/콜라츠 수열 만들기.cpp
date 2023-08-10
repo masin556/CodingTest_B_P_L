c35010af60f3a6efd6fb703eccb48358508d7eec
ppatabox <65951253+masin556@users.noreply.github.com>
2023-08-11 00:55:30 +0900

    [unrated] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    answer.push_back(n);
+    
+    while(n != 1)
+    {
+        if(n % 2 == 0)
+        {
+            n /= 2;
+        }
+        else
+        {
+            n = 3 * n + 1; 
+        }
+        answer.push_back(n);
+    }
+    
+    return answer;
+}
