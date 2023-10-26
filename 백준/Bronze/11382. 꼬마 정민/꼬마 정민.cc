8c110872e2e161539270381f4e911708ee1bb46c
ppatabox <65951253+masin556@users.noreply.github.com>
2023-10-26 22:32:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long A = 0, B = 0, C = 0;
+    cin >> A >> B >> C;
+    long long sum = A + B + C;
+    cout << sum << endl;
+    return 0;
+}
