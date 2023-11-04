7aa5a9bf39c3d6077a6b928aaaf7673c1b3c91d1
ppatabox <masin556@gmail.com>
2023-11-04 23:54:41 +0900

    :sparkles: feat : [23.11.04] [ppatabox] hackeranktest
    Hackerrank test

---
+++ HanckerRank/HackerRankCD/HackerRankCD/Basics/ForLoop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	string n[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+	cin >> a >> b;
+
+
+	for (int i = 0; i < sizeof(n); i++)
+	{
+		if (i <= )
+	}
+
+	return 0;
+}
