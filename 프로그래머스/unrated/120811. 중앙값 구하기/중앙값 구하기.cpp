05b3486236c99233a06a44454b80c30026335172
ppatabox <65951253+masin556@users.noreply.github.com>
2023-11-24 14:41:24 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> array) {
+    sort(array.begin(), array.end());
+
+    return array[array.size()/2];
+}
