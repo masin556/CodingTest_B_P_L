2c326a8cb271bb746c08b08c902fa422d2005514
ppatabox <65951253+masin556@users.noreply.github.com>
2023-11-26 00:33:18 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.cpp
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -4,20 +4,26 @@
 using namespace std;
 
 int solution(vector<int> array) {
-    short arr[1001] = {0, };
-    for (int i = 0; i < array.size(); i++) {
+    //short 타입) 배열크기 1001 초기값 0 이: 입력 배열의 각 숫자 빈도수 저장
+    short arr[1001] = {0};
+    
+    /*
+    현재 까지 최대 빈도수 최대 빈도수 가지는 숫자의 index를 저장 한다. 
+    cnt : 최대 빈도수를 가지는 숫자의 갯수
+    */
+    int maxFreq = 0, index = 0, cnt = 0;
+    for (int i = 0; i < array.size(); i++) 
+    {
+        //숫자 빈도 arr에 저장
         arr[array[i]]++;
-    }
-    
-    int maxFreq = 0, index = 0;
-    int cnt = 0;
-    
-    for (int i = 0; i < 1001; i++) {
-        if (arr[i] > maxFreq) {
-            maxFreq = arr[i];
+         if (arr[array[i]] > maxFreq) 
+        {
+            maxFreq = arr[array[i]];
             cnt = 1;
-            index = i;
-        } else if (arr[i] == maxFreq) {
+            index = array[i];
+        } 
+        else if (arr[array[i]] == maxFreq) 
+        {
             cnt++;
         }
     }
