36275f044522d7b86be6474624f5260e8aca8ff0
ppatabox <65951253+masin556@users.noreply.github.com>
2023-11-26 00:35:26 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/README.md
+++ 프로그래머스/unrated/120812. 최빈값 구하기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 최빈값 구하기 - 120812 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120812?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 3.63 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 0일 0:33:16
+2023년 11월 0일 0:35:25
 
 ### 문제 설명
 
--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.cpp
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.cpp
@@ -3,30 +3,32 @@
 
 using namespace std;
 
-int solution(vector<int> array) {
-    //short 타입) 배열크기 1001 초기값 0 이: 입력 배열의 각 숫자 빈도수 저장
+int solution(vector<int> array) 
+{
+    // 각 숫자의 빈도수를 저장할 배열 선언
     short arr[1001] = {0};
     
-    /*
-    현재 까지 최대 빈도수 최대 빈도수 가지는 숫자의 index를 저장 한다. 
-    cnt : 최대 빈도수를 가지는 숫자의 갯수
-    */
+    // 최빈값을 찾기 위한 변수들
     int maxFreq = 0, index = 0, cnt = 0;
+
+    // 주어진 배열을 순회하면서 각 숫자의 빈도수를 계산
     for (int i = 0; i < array.size(); i++) 
     {
-        //숫자 빈도 arr에 저장
-        arr[array[i]]++;
-         if (arr[array[i]] > maxFreq) 
+        arr[array[i]]++; // 현재 숫자의 빈도수 증가
+
+        // 최빈값 갱신 여부 확인
+        if (arr[array[i]] > maxFreq) 
         {
-            maxFreq = arr[array[i]];
-            cnt = 1;
-            index = array[i];
+            maxFreq = arr[array[i]]; // 최빈값 갱신
+            cnt = 1; // 최빈값의 개수 초기화
+            index = array[i]; // 최빈값의 인덱스 저장
         } 
         else if (arr[array[i]] == maxFreq) 
         {
-            cnt++;
+            cnt++; // 최빈값의 개수 증가
         }
     }
-    
+
+    // 최빈값이 여러 개라면 -1, 그렇지 않으면 최빈값 반환
     return (cnt != 1) ? -1 : index;
 }
