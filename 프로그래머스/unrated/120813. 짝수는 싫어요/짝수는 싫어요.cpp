031b76b1d9b351fbf517b3a33d9958605cef9c2b
ppatabox <65951253+masin556@users.noreply.github.com>
2023-11-27 19:17:35 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    for (int i = 1; i <= n; i += 2)
+    {
+        answer.push_back(i);
+    }
+    
+    return answer;
+}
