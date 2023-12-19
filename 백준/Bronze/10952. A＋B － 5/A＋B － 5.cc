656643e78a245b542f9edddfe76369523796cde1
ppatabox <65951253+masin556@users.noreply.github.com>
2023-12-19 14:12:39 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int a = 1, b = 1;
+	while (a != 0 && b != 0)
+	{
+		cin >> a >> b;
+		if (a == 0 && b == 0)
+		{
+			break;
+		}
+		else
+		{
+			cout << a + b << endl;
+		}
+	}
+	return 0;
+}
