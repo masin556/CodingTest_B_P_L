3fdeeae190ce7ebf13bab5812d3177aa85c7f2b9
ppatabox <65951253+masin556@users.noreply.github.com>
2023-12-28 00:14:38 +0900

    [Silver IV] Title: 스택 2, Time: 152 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.cc
+++ 백준/Silver/28278. 스택 2/스택 2.cc
@@ -3,46 +3,39 @@
 
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int n;
-    cin >> n;
-    
-    stack<int> stack;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-    for (int i = 0; i < n; i++)
-    {
-        int cmd, x;
-        cin >> cmd;
+  int n;
+  cin >> n;
 
-        if (cmd == 1)
-        {
-            cin >> x;
-            stack.push(x);
-        }
-        else if (cmd == 2)
-        {
-            cout << (stack.empty() ? -1 : stack.top()) << '\n';
-            if (!stack.empty())
-                stack.pop();
-        }
-        else if (cmd == 3)
-        {
-            cout << stack.size() << '\n';
-        }
-        else if (cmd == 4)
-        {
-            cout << stack.empty() << '\n';
-        }
-        else if (cmd == 5)
-        {
-            cout << (stack.empty() ? -1 : stack.top()) << '\n';
-        }
+  stack<int> st;
+  while (n--) {
+    int cmd, x;
+    cin >> cmd;
+
+    switch (cmd) {
+      case 1:
+        cin >> x;
+        st.push(x);
+        break;
+      case 2:
+        cout << (st.empty() ? -1 : st.top()) << '\n';
+        if (!st.empty()) st.pop();
+        break;
+      case 3:
+        cout << st.size() << '\n';
+        break;
+      case 4:
+        cout << st.empty() << '\n';
+        break;
+      case 5:
+        cout << (st.empty() ? -1 : st.top()) << '\n';
+        break;
     }
+  }
 
-    return 0;
+  return 0;
 }
