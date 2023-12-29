3d63685abf5c8293d66fefcd785d07d5fa2d6391
ppatabox <65951253+masin556@users.noreply.github.com>
2023-12-29 23:22:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    char n;
+    cin >> n;
+    cout << (int)n << '\n';
+    
+    return 0;
+}
