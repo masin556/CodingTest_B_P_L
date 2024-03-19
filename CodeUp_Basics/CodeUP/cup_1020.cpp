45bfa592e3cd5c582202550b4ab6faff0b2e79f5
ppatabox <masin556@gmail.com>
2024-03-19 10:00:14 +0900

    :sparkles: feat [240319] [chungsik] "2020 solved"

---
+++ CodeUp_Basics/CodeUP/cup_1020.cpp
@@ -0,0 +1,25 @@
+/* https://codeup.kr/problem.php?id=1020 */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string address, em;
+	cin >> address;
+
+	for (int i = 0; i < address.size(); i++)
+	{
+		if (address[i] != '-')
+		{
+			em += address[i];
+		}
+	}
+
+	//address.erase(remove(address.begin(), address.end(), '-'), address.end());
+
+	cout << em;
+	
+}
