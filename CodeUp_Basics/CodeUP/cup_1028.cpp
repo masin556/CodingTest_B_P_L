759b723055bc8044d67d470ae5c40855decff122
ppatabox <masin556@gmail.com>
2024-05-10 21:37:03 +0900

    :sparkles: feat [240510] [chungsik] "CodeUp 100 basic"
    1028, 1029 solved

---
+++ CodeUp_Basics/CodeUP/cup_1028.cpp
@@ -0,0 +1,10 @@
+/* https://codeup.kr/problem.php?id=1028 */
+
+#include <iostream>
+
+int main()
+{
+	unsigned int num;
+	std::cin >> num;
+	std::cout << num << std::endl;
+}
---
+++ CodeUp_Basics/CodeUP/cup_1029.cpp
@@ -0,0 +1,9 @@
+/* https://codeup.kr/problem.php?id=1029 */
+#include <stdio.h>
+
+int main()
+{
+	double rnum;
+	scanf_s("%lf", &rnum);
+	printf("%.11lf", rnum);
+}
