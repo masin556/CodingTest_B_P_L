9d67af9b7d3138980171ea55a259a8ba1c2896a9
ppatabox <masin556@gmail.com>
2024-05-13 22:06:09 +0900

    :sparkles: feat [240513] [chungsik] "codeUP_1030 solved"

---
+++ CodeUp_Basics/CodeUP/cup_1030.cpp
@@ -0,0 +1,13 @@
+/* https://codeup.kr/problem.php?id=1030 */
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	long long n;
+	/*scanf_s("%lld", &n); 
+	printf("%lld", n);*/
+	std::cin >> n;
+	std::cout << (long long)n << std::endl;
+}
