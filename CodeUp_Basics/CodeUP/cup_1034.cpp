081f61b1bada1b5e499db23fda4c5349b5945665
ppatabox <masin556@gmail.com>
2024-06-29 22:40:46 +0900

    :sparkles: feat [240629] [chungsik] "oct->dec"

---
+++ CodeUp_Basics/CodeUP/cup_1034.cpp
@@ -0,0 +1,17 @@
+/**
+* https://codeup.kr/problem.php?id=1034
+* 8진수로 입력된 정수 1개를 10진수로 바꾸어 출력
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int octo;
+	cin >> oct >> octo;
+
+	cout << dec << octo;
+	return 0;
+}
