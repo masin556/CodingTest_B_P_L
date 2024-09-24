6948b7f8069504b0e7e2712a974fdf3287bc3be1
ppatabox <65951253+masin556@users.noreply.github.com>
2024-09-24 11:43:16 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n; 
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        
+        for (int k = n; k > i; k--)
+        {
+            cout << "*";
+        }
+        cout << '\n';
+    }
+    return 0; 
+}
