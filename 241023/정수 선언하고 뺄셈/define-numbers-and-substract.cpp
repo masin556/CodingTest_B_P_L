d0e1a34d9e9d960b58960c15fcf76aabf0ba97b1
ppatabox <65951253+masin556@users.noreply.github.com>
2024-10-23 00:07:42 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b; 
+    a = 97;
+    b = 13;
+    printf("%d - %d = %d", a, b, a-b);
+    return 0;
+}
