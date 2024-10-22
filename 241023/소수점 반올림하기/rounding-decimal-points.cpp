a4e120c71fb3fc5ef9a2fff844b9e6cc63a5c4a0
ppatabox <65951253+masin556@users.noreply.github.com>
2024-10-23 00:20:33 +0900

    Title: 소수점 반올림하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << fixed;
+    cout.precision(1);
+
+    float a = 25.352;
+    cout << a;
+    return 0;
+}
