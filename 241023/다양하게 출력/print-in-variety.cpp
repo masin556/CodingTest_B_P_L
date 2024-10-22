1764d3c2c0ca6d6cf3bbcddffb5a73ee82cceec8
ppatabox <65951253+masin556@users.noreply.github.com>
2024-10-23 00:05:53 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cout << "Total days in Year" << endl << 365 << endl <<"Circumference rate" << endl << "3.1415926535";
+    return 0;
+}
