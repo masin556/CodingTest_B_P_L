1901f4723fcc511c1d233e1de9fff1347d866c63
ppatabox <65951253+masin556@users.noreply.github.com>
2024-10-23 22:54:18 +0900

    Title: 정수 복사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/정수 복사/copy-integer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3, b = 4;
+    b = a;
+    cout << a << " "<<b << endl;
+    printf("%d", a*b);
+    return 0;
+}
