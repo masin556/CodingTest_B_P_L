7beea44bfcdef5be4faa68759fe2a9d6e48d4cb7
ppatabox <65951253+masin556@users.noreply.github.com>
2024-10-23 22:53:20 +0900

    Title: 변수 값 교체하기 5, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/변수 값 교체하기 5/replacing-variable-values-5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 2, b = 6;
+    a = b;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    return 0;
+}
